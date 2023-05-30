09c4862cbbd80f5293489938a1ebb10fada5ded3
ntsambani <ntsambani@fmsystems.com>
2023-05-30 11:46:50 +0200

    infinite query

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,33 +1,36 @@
+import React from "react";
 import { useState } from "react";
 import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
-  const [userId, setUserId] = useState<number>();
-  const { data: posts, error, isLoading } = usePosts(userId);
+  const pageSize = 10;
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } =
+    usePosts({ pageSize });
+
   if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error.message}</p>;
 
-  if (error) return <p>{error}</p>;
-
   return (
     <>
-      <select 
-      onChange={(event)=> setUserId(parseInt(event.target.value))}
-      value={userId}
-      className="form-select mb-3">
-        <option value=""></option>
-        <option value="1">User 1</option>
-        <option value="2">User 2</option>
-        <option value="3">User 3</option>
-      </select>
       <ul className="list-group">
-        {posts.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {data.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
+      <button
+        className="btn btn-primary my-3 ms-1"
+        disabled={isFetchingNextPage}
+        onClick={() => fetchNextPage()}
+      >
+        {isFetchingNextPage ? "Loading..." : "Load More"}
+      </button>
     </>
   );
 };
