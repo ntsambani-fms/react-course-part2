a7e6d3b814d9f2cc729b844d44804059e8c0740e
ntsambani <ntsambani@fmsystems.com>
2023-06-06 20:12:35 +0200

    creating reusable HTTP Service

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,22 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
-import APIClient from '../services/apiClient';
 import { CACHE_KEY_TODOS } from '../constants';
+import todoService, { Todo } from '../services/todoService';
 
-const apiClient = new APIClient<Todo>('/todos')
 
-export interface Todo {
-    id: number;
-    title: string;
-    userId: number;
-    completed: boolean;
-  }
-  
+
+
 
 const useTodos = () => {
     
       return  useQuery<Todo[], Error>({
         queryKey: CACHE_KEY_TODOS,
-        queryFn: apiClient.getAll,
+        queryFn: todoService.getAll,
         staleTime: 10 * 1000,
       });
     
